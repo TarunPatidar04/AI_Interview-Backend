8b6fabafac70058a74e0476712d6134299f20402
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-03-01 19:05:14 +0530

    feat: Introduce Google authentication, including user model, JWT token utility, and dedicated auth routes and controllers.

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
 connectDB();
-app.use(cors());
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true,
+    }
+));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("AI Interview Backend is running");
