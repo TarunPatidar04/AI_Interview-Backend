3f4f332eadd285d7d7e15095e5ab00b8c064bc6d
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-03-01 17:25:59 +0530

    feat: Add root endpoint and refactor database connection module to config directory.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import connectDB from "./utils/db.js";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,6 +11,9 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("AI Interview Backend is running");
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
