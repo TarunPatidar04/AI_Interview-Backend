8228cf5ddd1011623ba8d3ec0a598aa9554183a6
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-03-04 10:41:22 +0530

    feat: Implement AI-powered interview question generation, credit deduction, and persistence using a new Interview model.

---
+++ models/interview.model.js
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+    },
+    difficulty: {
+        type: String,
+    },
+    timeLimit: {
+        type: String,
+    },
+    answer: {
+        type: String,
+    },
+    feedback: {
+        type: String,
+    },
+    score: {
+        type: Number,
+    },
+    confidence: {
+        type: Number,
+        default: 0
+    },
+    communication: {
+        type: Number,
+        default: 0
+    },
+    correctness: {
+        type: Number,
+        default: 0
+    },
+
+});
+
+const interviewSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    experience: {
+        type: String,
+        required: true
+    },
+    mode: {
+        type: String,
+        enum: ["HR", "Technical"],
+        required: true
+    },
+    resumeText: {
+        type: String,
+        required: true
+    },
+    questions: [questionSchema],
+    finalScore: {
+        type: Number,
+        default: 0
+    },
+    status: {
+        type: String,
+        enum: ["incompleted", "completed"],
+        default: "incompleted"
+    }
+
+}, { timestamps: true });
+
+const Interview = mongoose.model("Interview", interviewSchema);
+
+export default Interview;
